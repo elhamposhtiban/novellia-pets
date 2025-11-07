95ee4356a4e67ccae9c0d3a1631bdf395b232d12
Elham Poshtiban <elham.poshtiban@gmail.com>
2025-11-07 00:28:52 -0600

    Add the react app and add a package.json to run server side and client side both

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Novellia Pets</h1>
+        <p>Medical records management for your furry friends</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
